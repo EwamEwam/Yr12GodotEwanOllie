1404cb35c4a8cf885548f31faed6f277e0a78a1c
ollie <ruobuc@gmail.com>
2025-06-12 15:00:48 +1200

    Revert "Merge branch 'Creating-Enemies'"
    This reverts commit 58df40a9c9a3eb80bc214c24d4ca7a035a4cdc48, reversing
    changes made to 4ce91cc9caac06696db80cf311cba2f07efe99fc.

---
+++ dumb-things-in-deep-water/Scenes/Level/World.gd
@@ -0,0 +1,7 @@
+extends CollisionShape3D
+
+@onready var player = $Player
+
+func _physics_process(delta):
+#	get_tree().call_group("enemies", "update_target_location", player.global_transformation.origin)
+	pass
