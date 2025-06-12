7518ca5b91bb5efb81ce781924a16cf9e82a5171
ollie <ruobuc@gmail.com>
2025-06-12 14:57:51 +1200

    hugely updated enemy AI

--- dumb-things-in-deep-water/Scripts/Characters/enemy.gd
+++ dumb-things-in-deep-water/Scripts/Characters/enemy.gd
@@ -2,22 +2,27 @@ extends CharacterBody3D
 
 @onready var nav_agent :NavigationAgent3D = $NavigationAgent3D
 
-var SPEED :float = 3.0
-var ACCEL :float = 10.0
+@export var SPEED :float = 4.0
+@export var ACCEL :float = 1.0
+
+@export var DETECTION_DISTANCE :float = 50.0
 
 func _physics_process(delta :float) -> void:
-	
-	var direction = Vector3()
-	
-	var current_location :Vector3 = global_transform.origin
-	direction = (nav_agent.get_next_path_position() - current_location).normalized()
-
-	velocity = velocity.lerp(direction * SPEED, ACCEL * delta)
 	if not is_on_floor():
 		velocity.y -= 0.9
 	else:
 		velocity.y = 0
-	move_and_slide()
+			
+	if (global_position - Playerstats.player.global_position).length() < DETECTION_DISTANCE:
+		var direction = Vector3()
+		
+		var current_location :Vector3 = global_transform.origin
+		direction = (nav_agent.get_next_path_position() - current_location).normalized()	
+	
+		velocity = velocity.lerp(direction * SPEED, ACCEL * delta)
+	else:
+		velocity = velocity.lerp(Vector3.ZERO, ACCEL*delta)
 
+	move_and_slide()
 func update_target_location(target_location :Vector3) -> void:
 	nav_agent.target_position = target_location
