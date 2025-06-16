86207826725aa766b0f17bad494f72cff8797d30
ollie <ruobuc@gmail.com>
2025-06-17 11:01:56 +1200

    updated player slighy]

--- dumb-things-in-deep-water/Scripts/Characters/enemy.gd
+++ dumb-things-in-deep-water/Scripts/Characters/enemy.gd
@@ -2,13 +2,14 @@ extends CharacterBody3D
 
 @onready var nav_agent :NavigationAgent3D = $NavigationAgent3D
 
-@export var SPEED :float = 40
-@export var ACCEL :float = 1.0
+@export var SPEED :float = 8
+@export var ACCEL :float = 3.0
 
 @export var DETECTION_DISTANCE :float = 50.0
-@export var MIN_DETECTION_DIST :float = 2.0
+@export var MIN_DETECTION_DIST :float = 3.0
 
 func _physics_process(delta :float) -> void:
+	
 	if not is_on_floor():
 		velocity.y -= 0.9
 	else:
@@ -20,16 +21,17 @@ func _physics_process(delta :float) -> void:
 		var direction = Vector3()
 		direction = (nav_agent.get_next_path_position() - global_position).normalized()
 		velocity = velocity.lerp(direction * SPEED, ACCEL * delta)
+		$Body.look_at(global_position + velocity)
 	
 	elif distance < MIN_DETECTION_DIST:
 		
-		var direction = Vector3()
-		direction = (nav_agent.get_next_path_position() - global_position).normalized()
 		velocity = velocity.lerp(Vector3.ZERO, delta*10)
-		
+		#$Body.look_at(Playerstats.player.global_position)
+		print(lerp)
 	else:
 		velocity = velocity.lerp(Vector3.ZERO, ACCEL*delta)
 
+	$Body.rotation = Vector3(0,$Body.rotation.y,0)
 	move_and_slide()
 	
 func update_target_location(target_location :Vector3) -> void:
@@ -37,7 +39,7 @@ func update_target_location(target_location :Vector3) -> void:
 
 func _on_hitbox_body_entered(body: Node3D) -> void:
 	if body.is_in_group("Player"):
-		await get_tree().create_timer(1).timeout
+		await get_tree().create_timer(0.1).timeout
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		OS.alert("On Grand Bibby, you are dead.")
 		get_tree().quit()
